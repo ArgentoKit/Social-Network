8858fe8ce697ceefdf103ee298a017043798cd43
Aleksandr Raiko <awisenone@gmail.com>
2021-06-29 19:12:13 +0300

    Add React.lazy, React.Suspense

--- social-network/src/components/Main/Main.js
+++ social-network/src/components/Main/Main.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import style from './Main.module.scss';
 import News from './News/News';
 import Music from './Music/Music';
 import Settings from './Settings/Settings';
-import DialogsContainer from './Dialogs/DialogsContainer';
 import UsersContainer from './Users/UsersContainer';
-import ProfileContainer from './Profile/ProfileContainer';
+import Preloader from '../common/Preloader/Preloader';
+const DialogsContainer = React.lazy(() => import('./Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'));
 
 const Main = () => {
     return (
         <main className={style.main}>
-            <Route path='/profile/:userId?' render={ () => <ProfileContainer />} />
-            <Route path='/dialogs' render={ () => <DialogsContainer />}/>
-            <Route path='/news' render={ () => <News /> } />
-            <Route path='/music' render={ () => <Music /> } />
-            <Route path='/users' render={ () => <UsersContainer /> } />
-            <Route path='/settings' render={ () => <Settings /> } />
+            <Route path='/profile/:userId?' render={() => {
+                return (
+                    <Suspense fallback={<Preloader />}>
+                        <ProfileContainer />
+                    </Suspense>
+                )
+            }} />
+            <Route path='/dialogs' render={() => {
+                return (
+                    <Suspense fallback={<Preloader />}>
+                        <DialogsContainer />
+                    </Suspense>
+                )
+            }} />
+            <Route path='/news' render={() => <News />} />
+            <Route path='/music' render={() => <Music />} />
+            <Route path='/users' render={() => <UsersContainer />} />
+            <Route path='/settings' render={() => <Settings />} />
         </main>
     );
 }
